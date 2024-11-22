639160b317ba500f0e66ef61d5bf71cbabbfd1e1
FedeWaelk <fedewaelk@gmail.com>
2024-11-22 11:30:10 -0300

    Write the logic to get the human choice and make the choice variable global

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 console.log("Hello World")
 
+const choice = ["rock", "paper", "scissors"]
+
 function getComputerChoice() {
-    const choices = ["rock", "paper", "scissors"]
-    return choices[Math.floor(Math.random() * 3)]
-};
+    return choice[Math.floor(Math.random() * 3)]
+}
 
 console.log(getComputerChoice())
 
+function getHumanChoice() {
+    let humanChoice;
+
+    do {
+        humanChoice = prompt("Choose an option: Rock (1), Paper (2), or Scissors (3)");
+    } while (humanChoice !== "1" && humanChoice !== "2" && humanChoice !== "3");
+
+    return choice[parseInt(humanChoice) - 1];
+}
+
+console.log(getHumanChoice())
